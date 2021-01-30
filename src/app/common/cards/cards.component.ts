f67fdae9c6c24c63ddb2efad25ad8b4608bbfae2
Kiss Ferenc <69081242+Frenczy@users.noreply.github.com>
2021-01-30 23:27:26 +0100

    Update cards.component.ts

--- src/app/common/cards/cards.component.ts
+++ src/app/common/cards/cards.component.ts
@@ -1,13 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductService} from 'src/app/service/product.service';
+import { Product } from 'src/app/model/product';
 
 @Component({
   selector: 'app-cards',
   templateUrl: './cards.component.html',
   styleUrls: ['./cards.component.scss']
 })
-export class CardsComponent implements OnInit {
+export class CardsComponent extends ProductService implements OnInit {
 
-  constructor() { }
+  listFeatured:Product[] = this.list.filter(e=>e.featured == true);
+  listNonFeatured:Product[] = this.list.filter(e=>e.featured == false);
+  
+  constructor() { super()}
 
   ngOnInit(): void {
   }
